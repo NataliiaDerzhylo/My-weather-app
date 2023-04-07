d537b48d1ec93fc9d1c74fb281faf2d4ed30f912
NataliiaDerzhylo <natali.dergilo@gmail.com>
2023-04-07 15:57:19 +0300

    Added the search engine

--- index.js
+++ index.js
@@ -42,7 +42,20 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "Sydney";
-let apiKey = "7dc301a446bb9379190bcbafb3374dfc";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+
+function search(city) {
+  let apiKey = "7dc301a446bb9379190bcbafb3374dfc";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
