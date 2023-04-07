2ea345644f7a7a41939285f972205e56c22ea496
NataliiaDerzhylo <natali.dergilo@gmail.com>
2023-04-07 15:31:44 +0300

    Added the relevant icon from the API

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-  function formatDate (timestamp){
-    let date = new Date(timestamp);
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -18,35 +18,31 @@
     "Saturday",
   ];
   let day = days[date.getDay()];
-    return `${day} ${hours}:${minutes}`;
+  return `${day} ${hours}:${minutes}`;
+}
 
-  }
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
-  function displayTemperature (response){
-
-    let temperatureElement = document.querySelector ("#temperature");
-      let cityElement = document.querySelector("#city");
-        let descriptionElement = document.querySelector("#description");
-          let humidityElement = document.querySelector("#humidity");
-            let windElement = document.querySelector("#wind");
-              let dateElement = document.querySelector("#date");
-
-
-
-
-
-temperatureElement.innerHTML = Math.round (response.data.main.temp);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
-    descriptionElement.innerHTML = response.data.weather[0].description;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
-      dateElement.innerHTML = formatDate(response.data.dt * 1000);
-
-
-
-
-  }
-  let city = "New York";
-  let apiKey = "7dc301a446bb9379190bcbafb3374dfc";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+let city = "Sydney";
+let apiKey = "7dc301a446bb9379190bcbafb3374dfc";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
